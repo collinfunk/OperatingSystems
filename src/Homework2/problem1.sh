af8054a6bf1a9cd66d8925109130627644413643
Collin <funkcollin@gmail.com>
2022-07-31 08:18:39 -0700

    Makefile

---
+++ src/Homework2/problem1.sh
@@ -0,0 +1,8 @@
+echo -n $"This program adds three numbers. Enter the first number: "
+read firstNum
+echo -n $"Enter the second number: "
+read secondNum
+echo -n $"Enter the third number: "
+read thirdNum
+echo -n $"The sum of these three numbers is "
+echo $((firstNum + secondNum + thirdNum))
