8b3b5d97e60fe10e45062106d2f40a6e42761fbc
Collin <funkcollin@gmail.com>
2022-07-31 08:20:22 -0700

    Removed old file

--- Homework2/problem1.sh
+++
@@ -1,8 +0,0 @@
-echo -n $"This program adds three numbers. Enter the first number: "
-read firstNum
-echo -n $"Enter the second number: "
-read secondNum
-echo -n $"Enter the third number: "
-read thirdNum
-echo -n $"The sum of these three numbers is "
-echo $((firstNum + secondNum + thirdNum))
