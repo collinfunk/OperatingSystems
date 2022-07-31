3c7f0944527304c85e2bbb9fb6b2b03b5b6a9696
Collin <funkcollin@gmail.com>
2022-07-31 05:29:56 -0700

    Gitignore, buffer complete, progress bankers algo

--- Homework7/buffer.c
+++ Homework7/buffer.c
@@ -10,8 +10,6 @@ pthread_mutex_t mutex;
 sem_t empty, full;
 int insertPos = 0, removePos = 0;
 
-
-
 int insert_item(buffer_item item) {
     int returnVal = 0;
     sem_wait(&full); // Wait until there is space in the buffer
@@ -31,7 +29,7 @@ int insert_item(buffer_item item) {
 
 int remove_item(buffer_item *item) {
     int returnVal = 0;
-    sem_wait(&empty);
+    sem_wait(&empty); // Wait until there is something in the buffer
     pthread_mutex_lock(&mutex);
 
     if (insertPos > 0) {
@@ -46,15 +44,15 @@ int remove_item(buffer_item *item) {
 }
 
 int initialize_buffer() {
-    sem_init(&empty, 0, 0);
-    sem_init(&full, 0, BUFFER_SIZE);
-    pthread_mutex_init(&mutex, NULL);
+    sem_init(&empty, 0, 0); // Initialize empty semaphore to 0
+    sem_init(&full, 0, BUFFER_SIZE); // Initialize full semaphore to BUFFER_SIZE
+    pthread_mutex_init(&mutex, NULL); 
     return 0;
 }
 
 int deinitialize_buffer() {
-    sem_destroy(&empty);
-    sem_destroy(&full);
+    sem_destroy(&empty); // Destroy empty semaphore
+    sem_destroy(&full); // Destroy full semaphore
     pthread_mutex_destroy(&mutex);
     return 0;
 }
